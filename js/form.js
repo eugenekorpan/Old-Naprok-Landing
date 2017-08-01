01c5c2c870fa9328a56e98b72f93ad2d1bd0efd7
Oleksandr <targonsky97@gmail.com>
2017-08-01 14:01:03 +0300

    iframe contact form

---
+++ js/form.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+var _formstruct = {
+    formId: '59805aa43f8f9aa0148b4567',
+    formTitle: 'Contact us',
+    buttonText: 'Обратная связь',
+    buttonTextColor: 'ffffff',
+    buttonBgColor: '0064CD',
+    position: 'right',
+    boxWidth: 600,
+    verticalOffset: 30,
+    baseUrl: 'https://formstruct.ru/'
+};
+
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript'; 
+    script.async = 'true';
+    script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 
+        'formstruct.ru/widget/widget-2.2.min.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+})();
+</script>
