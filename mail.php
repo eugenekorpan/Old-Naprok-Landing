8bf5b7b6a6354d81c0ffcd6fe44560c7aeb5bc5c
Oleksandr <targonsky97@gmail.com>
2017-08-14 18:42:50 +0300

    contact

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "targonsky97@gmail.com";
+$sitename = "Naprok.com";
+
+$fullname = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["message"]);
+$message = "Name: $name \nPhone: $phone \nMessage: $text";
+
+$pagetitle = "New message from Naprok \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
