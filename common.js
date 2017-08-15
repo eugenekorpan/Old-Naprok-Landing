467c5c4ed899d9c7798e84972807954d161e25e5
Oleksandr <targonsky97@gmail.com>
2017-08-15 17:00:02 +0300

    fix common.js

--- common.js
+++ common.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-    $("#form").on('submit', function(event) {
-        event.preventDefault(); 
-        var formData = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: 'mail.php',
-            dataType: "json",
-            data: formData,
-            success: function(response) { 
-                alert(response.success); 
-            },
-            error: function(xhr, status, error){
-                console.log(xhr); 
-            }
-        });
-    });
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("success!");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
 });
