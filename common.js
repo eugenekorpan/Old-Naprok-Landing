028d2f75bd3e735ec64ece56d47b2483d3dfef81
Oleksandr <targonsky97@gmail.com>
2017-08-16 16:20:23 +0300

    mail

--- common.js
+++ common.js
@@ -1,16 +1,12 @@
-$(document).ready(function() {
-
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			alert("success!");
-			$("#form").trigger("reset");
-		});
-		return false;
-	});
-	
-});
+$(document).ready(function(){
+    $("#form").submit(function() { 
+            var form_data = $(this).serialize(); 
+            $.ajax({
+            type: "POST", 
+            url: "mail.php", 
+            data: form_data,
+            success: function() {
+                   alert("success!");
+            });
+    });
+});    
