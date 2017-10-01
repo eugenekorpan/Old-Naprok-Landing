8c33dfca95514913e0e9fab37b69fbb85d0c84ee
Oleksandr <targonsky97@gmail.com>
2017-10-01 15:49:10 +0300

    remove js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
-      $('#fullpage').fullpage({
-        anchors: ['home', 'about', 'why', 'screening', 'partners', 'contact'],
-        sectionsColor: ['', '#6869BB', '#fff', '#6869BB', '#e7e6e6', '#6869BB'],
-        navigation: true,
-        slidesNavigation: true,
-        controlArrows: false,
-        menu: '#menu',
-        responsiveWidth: 900,
-        afterResponsive: function(isResponsive){
-          
-        }
+  (function($){
+    $(function() {
+      $('.menu__icon').on('click', function() {
+        $(this).closest('.menu').toggleClass('menu_state_open');
       });
-
-(function($){
-  $(function() {
-    $('.menu__icon').on('click', function() {
-      $(this).closest('.menu').toggleClass('menu_state_open');
     });
-  });
-})(jQuery);
+  })(jQuery);
 });
 
