87b8088f3ff41813e11b7de25033e625aa1ea07b
Oleksandr <targonsky97@gmail.com>
2017-10-02 20:10:22 +0300

    concat and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var sass = require('gulp-sass');
+var cssMin = require('gulp-css');
+var concat = require('gulp-concat');
  
-gulp.task('compress', function() {
+gulp.task('cssMinfy', function(){
+  return gulp.src([
+  		'./css/app.css',
+  		'./css/bootstrap-theme.min.css',
+  		'./css/bootstrap.min.css',
+  		'./css/bootstrap.min.css'
+	])
+  	.pipe(concat('app.css'))
+    .pipe(cssMin())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('compress-img', function() {
   gulp.src('images/*')
   .pipe(imagemin())
   .pipe(gulp.dest('images'))
