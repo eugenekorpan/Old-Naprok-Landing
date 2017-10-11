593cd10ac79e6ec044909001c4bc52b5b2cf7576
Oleksandr <targonsky97@gmail.com>
2017-10-11 11:20:03 +0300

    amp

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,66 @@ var sass = require('gulp-sass');
 var cssMin = require('gulp-css');
 var concat = require('gulp-concat');
 var uncss = require('gulp-uncss');
+var gulp = require('gulp');
+var purify = require('gulp-purifycss');
+var cleanCSS = require('gulp-clean-css');
+var htmlreplace = require('gulp-html-replace');
+var amphtmlValidator = require('amphtml-validator');
+var fs = require('fs');
 
+const BUILD_PATH = './build';
+const BUILD_HTML = BUILD_PATH + '/index.html';
+const SOURCE = {
+  'CSS': './html/css/style.css',
+  'AMPHTML': './amphtml/index.html',
+  'CLEANED_CSS': './amphtml/css/style.css'
+}
+
+// purify removes unused CSS classes
+gulp.task('purify', function() {
+  return gulp.src(SOURCE.CSS)
+    .pipe(purify([SOURCE.AMPHTML]))
+    .pipe(gulp.dest('./amphtml/css'));
+});
+
+// inline-css inserts the cleaned + minified CSS into HTML
+gulp.task('inline-css', ['purify'], function() {
+  return gulp.src(SOURCE.AMPHTML)
+    .pipe(htmlreplace({
+      'cssInline': {
+        'src': gulp.src(SOURCE.CLEANED_CSS).pipe(cleanCSS()),
+        'tpl': '<style amp-custom>%s</style>'
+      }
+    }))
+    .pipe(gulp.dest(BUILD_PATH));
+});
+
+// validate ensures the AMP HTML is valid
+gulp.task('validate', function() {
+  amphtmlValidator.getInstance().then(function (validator) {
+    var input = fs.readFileSync(BUILD_HTML, 'utf8');
+    var result = validator.validateString(input);
+    ((result.status === 'PASS') ? console.log : console.error)(BUILD_HTML + ": " + result.status);
+    for (var ii = 0; ii < result.errors.length; ii++) {
+      var error = result.errors[ii];
+      var msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+      if (error.specUrl !== null) {
+        msg += ' (see ' + error.specUrl + ')';
+      }
+      ((error.severity === 'ERROR') ? console.error : console.warn)(msg);
+    }
+  });
+});
 
 gulp.task('cssMinfy', function(){
   return gulp.src([
   		'./css/app.css',
   		'./css/bootstrap.min.css',
-      './css/animate.min.css'
+      /*'./css/animate.min.css'*/
 	])
   	.pipe(concat('style.css'))
-/*    .pipe(uncss({
-            html: ['index.html']
-        }))*/
-    .pipe(cssMin())
-    .pipe(gulp.dest('./css'));
+    /*.pipe(gulp.dest('./css'));*/
+    .pipe(gulp.dest('./amphtml/css'));
 });
 
 /*gulp.task('scripts', function(){
@@ -44,4 +90,4 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass','cssMinfy', 'compress-img']);
+gulp.task('default', ['sass', 'cssMinfy','validate','inline-css']);
